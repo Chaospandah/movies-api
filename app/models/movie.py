42c6a1a3595d537e9fb10efa0cf5f3e4a5cc8a06
Chaospandah <harnoorsingh3943@gmail.com>
2025-12-05 20:37:45 -0800

    Added docs folder, fixed endpoint errors

--- app/models/movie.py
+++ app/models/movie.py
@@ -1,23 +1,26 @@
-from pydantic import BaseModel
 from typing import List, Optional
+
+from pydantic import BaseModel
+
 
 class MovieBase(BaseModel):
     title: str
     release_year: int
-    genre: List[str]
+    genres: List[str]
     summary: str
+
 
 class MovieCreate(MovieBase):
     pass
 
+
 class MovieUpdate(BaseModel):
     title: Optional[str] = None
-    relase_year: Optional[int] = None
-    genre: Optional[List[str]] = None
+    release_year: Optional[int] = None
+    genres: Optional[List[str]] = None
     summary: Optional[str] = None
+
 
 class Movie(MovieBase):
     id: int
     rating: Optional[float] = None
-
-
